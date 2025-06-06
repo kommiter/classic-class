ca8d2f214e2f578e110558be77c84fda5e79d462
kommiter <wnsgus821@gmail.com>
2025-06-06 19:05:56 +0900

    [FEAT] 화면 최소 사이즈 지정

--- src/Lobby.java
+++ src/Lobby.java
@@ -1,6 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
 
 public class Lobby {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
+        JFrame frame = new JFrame("Classic Class - Size Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(1024, 600));
+        frame.setSize(1024, 600);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
     }
 }
